dc45b62b99c12d8739682325c05af95faba8f644
Elisey Zanko <elisey.zanko@gmail.com>
2018-03-25 15:27:22 +0500

    Upgrade webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,11 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+    mode: 'production',
     plugins: [
-        new UglifyJSPlugin({
-            sourceMap: true,
-        }),
         new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production'),
-            },
             OAUTH_GATEWAY_URL: JSON.stringify('https://3fyst874r0.execute-api.eu-central-1.amazonaws.com/prod'),
             OAUTH_CLIENT_ID: JSON.stringify('5d3995b225dc40b5601b'),
         }),
