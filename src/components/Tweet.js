253f323d1023d7092061b9a7b95134cafd416714
Elisey Zanko <elisey.zanko@gmail.com>
2019-05-17 16:48:35 +0500

    Fix #61: Omit Twitter errors (#64)
    * Fix sorting issue
    * Upgrade packages

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 export default class Tweet extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: false,
+        };
+    }
+
     componentDidMount() {
-        twttr.widgets.load(); // eslint-disable-line no-undef
+        try {
+            twttr.widgets.load(); // eslint-disable-line no-undef
+        } catch (e) {
+            this.setState({error: true});
+        }
     }
 
     render() {
-        return (
+        return !this.state.error && (
             <div>
                 <a href="https://twitter.com/share?ref_src=twsrc%5Etfw"
                     className="twitter-share-button"
