6f2f8a7a6e072c86ef79f0ed98122cbb4a4a7edd
Ivan Gromov <summer.is.gone@gmail.com>
2019-10-22 01:21:48 +0500

    Improved mobile layout

--- src/components/MarkGitHub.js
+++ src/components/MarkGitHub.js
@@ -5,7 +5,7 @@ import MarkIcon from 'octicons/build/svg/mark-github.svg';
 export default class MarkGitHub extends React.PureComponent {
     render() {
         return (
-            <div>
+            <div className="hide-sm">
                 <a href={'https://github.com/my-contributions/my-contributions.github.io'}>
                     <MarkIcon width={22} height={22}/>
                 </a>
--- src/components/Results.js
+++ src/components/Results.js
@@ -12,7 +12,7 @@ export default class Results extends React.PureComponent {
         return (
             <div className="results">
                 <Author github={this.props.github}/>
-                <div className="contributions">
+                <div className="contributions pl-md-4 px-sm-2">
                     <PullRequests github={this.props.github}/>
                     <Issues github={this.props.github}/>
                 </div>
